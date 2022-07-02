4d7f04df4b0d52834daf079b8b917c76c6ea81a9
Shilpi <ssinha2@atlassian.com>
2022-07-03 05:24:42 +0530

    feat: Configure aadhar

---
+++ components/InitializeEthers.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ethers } from "ethers";
+import RedPetalsArtiFact from "../contracts/RedPetals.json";
+import ContractAddress from "../contracts/contract-address.json";
+
+const InitializeEthers = () => {
+  const [redPetalsData, setReadPetalsData] = React.useState({});
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  const redPetals = new ethers.Contract(
+    ContractAddress.RedPetals,
+    RedPetalsArtiFact.abi,
+    provider.getSigner(0)
+  );
+
+  React.useEffect(() => {
+    setReadPetalsData(redPetals);
+  }, []);
+
+  console.log(redPetalsData);
+  return <div>Hello</div>;
+};
+
+export default InitializeEthers;
--- components/loginPage.js
+++ components/loginPage.js
@@ -1,10 +1,12 @@
-import { Button, Box } from "native-base";
+import { Button, Box, Input, VStack, Heading } from "native-base";
 import { ethers } from "ethers";
 import HomePage from "./HomePage";
+import InitializeEthers from "./InitializeEthers";
 import React from "react";
 
 const Login = () => {
   const [homePage, setHomePage] = React.useState(false);
+  const [aadhar, setAadhar] = React.useState();
   const handleClick = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -15,17 +17,36 @@ const Login = () => {
     // send ether and pay to change state within the blockchain.
     // For this, you need the account signer...
     const signer = provider.getSigner();
-    console.log("helloo", signer);
     if (signer) {
       setHomePage(true);
     }
   };
 
+  const getInputValue = (event) => {
+    // show the user input value to console
+    const userValue = event.target.value;
+    setAadhar(userValue);
+  };
+
+  console.log(aadhar);
+
   return (
     <>
+      <InitializeEthers />
       <Box alignItems="center">
         {!homePage && (
-          <Button onPress={() => handleClick()}>Login/Register</Button>
+          <>
+            <VStack space={4} alignItems="center" width="300px">
+              <Heading>Verify yourself</Heading>
+              <Input
+                mx="3"
+                placeholder="Aadhar number"
+                w="100%"
+                onChange={getInputValue}
+              />
+              <Button onPress={() => handleClick()}>Login/Register</Button>
+            </VStack>
+          </>
         )}
         {homePage && <HomePage />}
       </Box>
